427f7b65551a617279ec0c5a154d7c4fdf99cb7d
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-07 20:22:14 +0100

    The shooting mechanic is now fixed.
    Now you can move the player and the rockets don't move with them as well.

--- positive-shooter-game/scripts/player.gd
+++ positive-shooter-game/scripts/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 var speed = 300
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
+var rocket_container
+
+func _ready():
+	rocket_container = get_node("RocketContainer")
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -25,5 +29,6 @@ func _physics_process(delta):
 
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
