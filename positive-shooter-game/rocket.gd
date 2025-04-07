dd7b8943909e6acc7e3fa4276240de870c483067
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-07 20:15:01 +0100

    The player can now shoot rockets
    Now the player can shoot the rockets, but there is still an issue, when the player moves, they also move with him.

--- positive-shooter-game/rocket.gd
+++ positive-shooter-game/rocket.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 4
+@export var speed = 300
 
 func _physics_process(delta):
-	global_position.x = global_position.x + speed
+	global_position.x = global_position.x + speed*delta
--- positive-shooter-game/scripts/player.gd
+++ positive-shooter-game/scripts/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 var speed = 300
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
 func _physics_process(delta):
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("move_right"):
@@ -16,3 +22,8 @@ func _physics_process(delta):
 
 	var screen_size = get_viewport_rect().size
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 80
