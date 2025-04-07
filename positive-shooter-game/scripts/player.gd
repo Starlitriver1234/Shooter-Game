10f806f685a5a3fb947f2dbb2f4bb3ce1019ac4e
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-07 11:50:34 +0100

    fixed the player stutter completely and made it so you can't exit the level anymore

--- positive-shooter-game/scripts/player.gd
+++ positive-shooter-game/scripts/player.gd
@@ -13,3 +13,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
 	move_and_slide()
+
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
