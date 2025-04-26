f7a165bd257a68c359cfc71650efa792f1ff3641
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-26 18:17:07 +0100

    Added the lives UI and started working on making it work
    So far, we have the UI on the screen and a little bit of the code needed to make the number update.

--- positive-shooter-game/scripts/hud.gd
+++ positive-shooter-game/scripts/hud.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var score = $Score
+@onready var lives_left = $LivesLeft
 
 func set_score_label(new_score):
 	score.text = "Score: " + str(new_score)
+
+func set_lives(ammount):
+	lives_left.text = str(ammount)
