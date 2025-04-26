876c842eddfab6028be45bdf406cf4c0f07b7755
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-26 17:02:45 +0100

    Score UI now gets updated with each new saving of an enemy

--- positive-shooter-game/scripts/game.gd
+++ positive-shooter-game/scripts/game.gd
@@ -3,6 +3,10 @@ extends Node2D
 var lives = 4
 var score = 0
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	hud.set_score_label(score)
 
 func _on_deathzone_area_entered(area: Area2D) -> void:
 	area.saved()
@@ -21,4 +25,5 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
 
 func _on_enemy_saved():
 	score = score + 100;
+	hud.set_score_label(score)
 	
