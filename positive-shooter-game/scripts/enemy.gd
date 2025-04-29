6d0676e51c7b0c155ef225fbbf5b9e503921a64d
Starlitriver1234 <C24745999@mytudublin.ie>
2025-04-29 15:14:42 +0100

    The ghosts change to a happy ghost once hit by the player
    I finally added one of the biggest mechanics of the game, the saving of the ghosts. You send their memories at them and turn them from aimless spirits back to who they are. They stay like this for a second before dissapearing. But I still have one issue, you can still be hit by the happy ghosts

--- positive-shooter-game/scripts/enemy.gd
+++ positive-shooter-game/scripts/enemy.gd
@@ -3,13 +3,36 @@ extends Area2D
 signal was_saved
 
 @export var speed = 300
+@export var duration: float = 1.0
+
+var new_ghost: Texture2D
+var _sprite: Sprite2D
+
+
+func _ready() -> void:
+	new_ghost = load("res://New Assets/Happy Ghost.png")
+	for child in get_children():
+		if child is Sprite2D:
+			_sprite = child
+			break
 
 func _physics_process(delta):
 	global_position.x += -speed * delta
+
+
 func saved():
+	if _sprite and new_ghost:
+		_sprite.texture = new_ghost
+		call_deferred("delayed")
+	else:
+		queue_free()
+
+
+func delayed():
+	await get_tree().create_timer(duration).timeout
 	emit_signal("was_saved")
 	queue_free()
-
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	body.take_damage()
